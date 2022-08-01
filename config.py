6c52ecb48af23f370c01bda2d16ecd4aa4eb528c
cloudpane01 <109420156+cloudpane01@users.noreply.github.com>
2022-08-01 20:45:07 +0000

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-token = 'API'
-admin = ['ID'] - сюда вписываем ID с телеграмма
+token = '5558301746:AAFspW2vVXfmA7OLMK3GkQW4hiPyB6vfwWk'
+admin = ['5301938332']
 
 dir1 = '/var/www/'
 dir_backup = '/backup/' #путь к папке с файлом бэкапа
